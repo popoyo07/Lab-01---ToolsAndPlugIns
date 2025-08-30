175c4c639c32a0d65285bd98fd6fc33767d82bdb
Sze <sz982600@ucf.edu>
2025-08-30 01:30:09 -0400

    idk the knightttttttttttt

--- Assets/Scripts/ChessType.cs
+++ Assets/Scripts/ChessType.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.U2D;
 
 public class ChessType : MonoBehaviour
 {
+    public int range = 1;
+
     public enum TypeOfChess
     {
         King,
@@ -14,29 +18,52 @@ public class ChessType : MonoBehaviour
 
     public TypeOfChess CurrentChess;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnDrawGizmosSelected()
     {
+    Gizmos.color = Color.yellow;
+
         switch (CurrentChess)
         {
             case TypeOfChess.King:
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.left * 1);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.right * 1);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.up * 1);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 1);
+                Gizmos.DrawRay(transform.position, new Vector3(1, 1, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(-1, -1, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(-1, 1, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(1, -1, 0));
                 break;
             case TypeOfChess.Queen:
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.left * range);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.right * range);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.up * range);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.down * range);
+                Gizmos.DrawRay(transform.position, new Vector3(range, range, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(-range, -range, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(-range, range, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(range, -range, 0));
                 break;
-            case TypeOfChess.Bishop: 
+            case TypeOfChess.Bishop:
+                Gizmos.DrawRay(transform.position, new Vector3(range, range, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(-range, -range, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(-range, range, 0));
+                Gizmos.DrawRay(transform.position, new Vector3(range, -range, 0));
                 break;
-            case TypeOfChess.Pawn: 
+            case TypeOfChess.Pawn:
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.up * 1);
                 break;
-            case TypeOfChess.Rook: 
+            case TypeOfChess.Rook:
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.left * range);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.right * range);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.up * range);
+                Gizmos.DrawLine(transform.position, transform.position + Vector3.down * range);
                 break;
-            case TypeOfChess.Knights: 
+            case TypeOfChess.Knights:
+
+                //IDK IDK IDK IDK aaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhh
+                
                 break;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
